2a745ad113ef2447b4fa6728b6867ad01d86400a
nazarBl <nazar1998777718@gmail.com>
2023-03-08 16:48:35 +0200

    Created User model for DB

---
+++ models/User.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+const UserSchem = new mongoose.Schema({
+        name:{
+            type:String,
+            required: true,
+        },
+        email:{
+            type:String,
+            unique:true,
+            required:true,
+        },
+        password:{
+            type:String,
+            required: true,
+        },
+        avatar:String,
+},{
+    timestamps:true
+})
+
+module.exports = mongoose.model('User', UserSchem)
