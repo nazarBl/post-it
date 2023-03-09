68e84106c56647e795c5ceff28a05a291410d1e2
nazarBl <nazar1998777718@gmail.com>
2023-03-09 20:45:48 +0200

    Send registered user data to mongoDB

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const jwt = require('jsonwebtoken')
+const bcrypt =require('bcrypt')
 const mongoose = require('mongoose')
 const registerValidation = require('./validations/auth')
 const {validationResult} = require('express-validator')
@@ -7,7 +8,7 @@ const {validationResult} = require('express-validator')
 const UserModel = require('./models/User')
 
 const PORT = 7000;
-mongoose.connect('mongodb+srv://Admin:123123123@cluster0.afio7fk.mongodb.net/?retryWrites=true&w=majority').then(()=>{
+mongoose.connect('mongodb+srv://Admin:123123123@cluster0.afio7fk.mongodb.net/post-it?retryWrites=true&w=majority').then(()=>{
     console.log('DB connected');
 }).catch((err)=>{
     console.log('DB connection error', err);
@@ -26,19 +27,19 @@ app.post('/auth/registration', registerValidation, async (req,res)=>{
         return res.status(400).json(errors.array())
     }
     const password = req.body.password;
-     const salt = await bcrypt.genSalt(10);
+    const salt = await bcrypt.genSalt(10);
     const passwordHash = await bcrypt.hash(password, salt);
 
     const doc = new UserModel({
         email:req.body.email,
         name:req.body.name,  
-        passwordHash,
+        password: passwordHash,
         avatarUrl:req.body.avatarUrl,
     })
 
-    res.json({
-        success:true,
-    });
+    const user = await doc.save()
+
+    res.json(user);
 })
 
 app.listen(PORT, (err)=>{
