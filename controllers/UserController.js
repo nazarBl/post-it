de99eadf2f9159992265a64c7a9e63808be6ab26
nazarBl <nazar1998777718@gmail.com>
2023-03-11 18:12:09 +0200

    Refactoring (UserController)

---
+++ controllers/UserController.js
@@ -0,0 +1,103 @@
+const jwt = require('jsonwebtoken')
+const bcrypt =require('bcrypt')
+
+const {validationResult} = require('express-validator')
+const UserModel = require('../models/User')
+module.exports = {
+    register : async (req,res)=>{
+        try {
+            const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json(errors.array())
+        }
+        const userPassword = req.body.password;
+        const salt = await bcrypt.genSalt(10);
+        const passwordHash = await bcrypt.hash(userPassword, salt);
+    
+        const doc = new UserModel({
+            email:req.body.email,
+            name:req.body.name,  
+            password: passwordHash,
+            avatarUrl:req.body.avatarUrl,
+        })
+    
+        const user = await doc.save()
+    
+        const token = jwt.sign({
+            _id: user._id,
+        },
+        'secretKiey',
+        {
+            expiresIn:'30d',
+        })
+    
+        const {password, ...userData} = user._doc
+    
+        res.json({
+            ...userData,
+            token
+        });
+        } catch (error) {
+            console.log(error);
+            res.status(500).json({
+                message:'Registration failed!'
+            })
+        }
+    },
+    
+    login : async (req,res)=>{
+        try {
+            const user = await UserModel.findOne({ email:req.body.email })
+            if(!user){
+                return res.status(404).json({
+                    message:'Wrong login or password!'
+                })
+            }
+    
+            const isValidPass = bcrypt.compare(req.body.password, user._doc.password)
+    
+            if (!isValidPass){
+                return res.status(404).json({
+                    message:'Wrong login or password!'
+                })
+            }
+    
+            const token = jwt.sign({
+                _id: user._id,
+            },
+            'secretKiey',
+            {
+                expiresIn:'30d',
+            })
+        
+            const {password, ...userData} = user._doc
+        
+            res.json({
+                ...userData,
+                token
+            });
+        } catch (error) {
+            res.status(500).json({
+                message:'Authorization failed!'
+            })
+        }
+    },
+    
+    getMe : async (req,res)=>{
+        try {
+            const user = await UserModel.findById(req.userId)
+          
+    
+            if(!user){
+                return res.status(404).json({
+                    message:`User not found!`
+                })
+            }
+            const {password, ...userData} = user._doc
+    
+        res.json(userData);
+        } catch (error) {}
+    }
+}
+
+
--- index.js
+++ index.js
@@ -1,19 +1,17 @@
 const express = require('express')
-const jwt = require('jsonwebtoken')
-const bcrypt =require('bcrypt')
 const mongoose = require('mongoose')
 const registerValidation = require('./validations/auth')
-const {validationResult} = require('express-validator')
-
-const UserModel = require('./models/User')
 const checkAuth = require('./middlewares/checkAuth')
+const {register,login,getMe} = require('./controllers/UserController.js')
 
 const PORT = 7000;
-mongoose.connect('mongodb+srv://Admin:123123123@cluster0.afio7fk.mongodb.net/post-it?retryWrites=true&w=majority').then(()=>{
+
+mongoose.connect('mongodb+srv://Admin:123123123@cluster0.afio7fk.mongodb.net/?retryWrites=true&w=majority').then(()=>{
     console.log('DB connected');
 }).catch((err)=>{
     console.log('DB connection error', err);
 })
+
 const app = express();
 
 app.use(express.json());
@@ -22,100 +20,10 @@ app.get('/',(req,res)=>{
     res.send('Home page')
 })
 
-app.post('/auth/login', async (req,res)=>{
-    try {
-        const user = await UserModel.findOne({ email:req.body.email })
-        if(!user){
-            return res.status(404).json({
-                message:'Wrong login or password!'
-            })
-        }
+app.post('/auth/registration', registerValidation, register)
+app.post('/auth/login', login)
+app.get('/auth/me', checkAuth, getMe)
 
-        const isValidPass = bcrypt.compare(req.body.password, user._doc.password)
-
-        if (!isValidPass){
-            return res.status(404).json({
-                message:'Wrong login or password!'
-            })
-        }
-
-        const token = jwt.sign({
-            _id: user._id,
-        },
-        'secretKiey',
-        {
-            expiresIn:'30d',
-        })
-    
-        const {password, ...userData} = user._doc
-    
-        res.json({
-            ...userData,
-            token
-        });
-    } catch (error) {
-        res.status(500).json({
-            message:'Authorization failed!'
-        })
-    }
-})
-
-app.post('/auth/registration', registerValidation, async (req,res)=>{
-    try {
-        const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-        return res.status(400).json(errors.array())
-    }
-    const userPassword = req.body.password;
-    const salt = await bcrypt.genSalt(10);
-    const passwordHash = await bcrypt.hash(userPassword, salt);
-
-    const doc = new UserModel({
-        email:req.body.email,
-        name:req.body.name,  
-        password: passwordHash,
-        avatarUrl:req.body.avatarUrl,
-    })
-
-    const user = await doc.save()
-
-    const token = jwt.sign({
-        _id: user._id,
-    },
-    'secretKiey',
-    {
-        expiresIn:'30d',
-    })
-
-    const {password, ...userData} = user._doc
-
-    res.json({
-        ...userData,
-        token
-    });
-    } catch (error) {
-        console.log(error);
-        res.status(500).json({
-            message:'Registration failed!'
-        })
-    }
-})
-
-app.get('/auth/me', checkAuth, async (req,res)=>{
-    try {
-        const user = await UserModel.findById(req.userId)
-      
-
-        if(!user){
-            return res.status(404).json({
-                message:`User not found!`
-            })
-        }
-        const {password, ...userData} = user._doc
-
-    res.json(userData);
-    } catch (error) {}
-})
 app.listen(PORT, (err)=>{
     
     if(err){
