92a9fe4f3e614972c36bb292b61f4923b82a2184
nazarBl <nazar1998777718@gmail.com>
2023-03-15 17:44:01 +0200

    Implement custom theme, index.scss

--- client/src/index.js
+++ client/src/index.js
@@ -2,10 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
+import { CssBaseline, ThemeProvider } from '@mui/material';
+import {theme} from './theme.js'
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-      <App />
+    <CssBaseline>
+      <ThemeProvider theme = {theme}>
+        <App />
+      </ThemeProvider>
+    </CssBaseline>
   </React.StrictMode>
 
 );
