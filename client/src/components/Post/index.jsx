10bef5a831c0be87e6398b70847cf6e8540c58e7
nazarBl <nazar1998777718@gmail.com>
2023-03-17 12:35:19 +0200

    Used clsx to make Post usable in Home and FullPost

--- client/src/components/Post/index.jsx
+++ client/src/components/Post/index.jsx
@@ -1,23 +1,28 @@
-import React from 'react'
-import { UserInfo } from '../UserInfo'
+import React from 'react';
+import { UserInfo } from '../UserInfo';
 import EyeIcon from "@mui/icons-material/RemoveRedEyeOutlined";
 import CommentIcon from "@mui/icons-material/ChatBubbleOutlineOutlined";
+import clsx from 'clsx';
 
-import style from './Post.module.scss'
+import style from './Post.module.scss';
 
-export const Post = ({_id, title, text, imageUrl, author, createdAt, tags, viewsCount, commentsCount}) => {
+export const Post = ({_id, title, text, imageUrl, author, createdAt, tags, viewsCount, commentsCount, children, isFullPost}) => {
   return (
-    <div className={style.root}>
-        <img className={style.postImage} src ={imageUrl} alt='title'/>
+    <div className={clsx(style.root, { [style.rootFull]: isFullPost })}>
+        <img className={clsx(style.image, { [style.imageFull]: isFullPost })} src ={imageUrl} alt='title'/>
         <div className={style.wrapper}>
           <UserInfo {...author} extraInfo={createdAt} />
-          <div className={style.indention}>
-              <h2 className={style.title}>{title}</h2>
+          <div className={style.indention}> 
+              <h2 className={clsx(style.title, { [style.titleFull]: isFullPost })}>
+                {isFullPost ? title : <a href={`/posts/${_id}`}>{title}</a>}
+              </h2>
               <ul className={style.tags}>
                   {tags.map((name)=>(
-                      <li key={name}>#{name}</li>
+                   
+                      <li key={name}> <a href={`/tag/${name}`}>#{name}</a></li>
                   ))}
               </ul>
+              {children && <div className={style.content}>{children}</div>}
               <ul className ={style.postDetails}>
                 <li>
                   <EyeIcon />
