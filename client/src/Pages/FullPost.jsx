dac58d31e64aa6cf503e7e4f54c5c2b1310a2f7c
nazarBl <nazar1998777718@gmail.com>
2023-03-20 15:52:48 +0200

    FullPost connected to backend

--- client/src/Pages/FullPost.jsx
+++ client/src/Pages/FullPost.jsx
@@ -3,23 +3,41 @@ import React from 'react';
 import { Index } from '../components/AddComment/';
 import { Post } from "../components/Post";
 import { CommentsBlock } from "../components/CommentsBlock";
+import { useParams } from 'react-router-dom';
+import axios from '../axios.js';
 
 export const FullPost = () => {
+  const [post, setPost] = React.useState();
+  const [isLoading, setIsLoading] = React.useState(true);
+
+  const {id} = useParams();
+  React.useEffect(()=>{
+    axios.get(`/posts/${id}`).then(res=>{
+      setPost(res.data);
+      setIsLoading(false)
+    }).catch((err)=>{
+      console.warn(err);
+      alert('Error while getting post') 
+    })
+  },[])
+
+  if (isLoading) {
+    return <Post isLoading={isLoading} isFullPost/>
+  }
+
   return (
     <>  
         <Post
-        _id={2}
-        title="Vacation in Malaysia"
-        imageUrl="https://www.oyorooms.com/blog/wp-content/uploads/2018/06/Featured-Image-Relaxing-places-of-Malaysia.jpg"
-        author={{
-            avatarUrl:"https://mui.com/static/images/avatar/7.jpg",
-            fullName:"Murphy",}}
-        createdAt={"12 June 2023"}
-        viewsCount={150}
-        commentsCount={3}
-        tags={["react", "fun", "typescript"]}
+        _id={post.id}
+        title={post.title}
+        imageUrl={post.imageUrl}
+        author={post.author}
+        createdAt={post.createdAt}
+        viewsCount={post.viewsCount}
+        commentsCount={post.commentsCount}
+        tags={post.tags}
         >
-        <p>So I am finnally here in Malaysia! If someone would tell me before I will spend my vacation in place like this i would never believe them! Luckily for me I get opportunity to take tickets at extra low price so I think "Why not, Murphy?" This place is amazing, people are so kind, food is delicious, but what really made me excited is those jaw-dropping views including one from my apartment. I am so happy not to miss chance be in Malasyia this week!</p>
+        <p>{post.text}</p>
         </Post>
         
             <CommentsBlock items={[
--- client/src/Pages/Home.jsx
+++ client/src/Pages/Home.jsx
@@ -11,8 +11,6 @@ export const Home = () => {
   const dispatch = useDispatch();
   const {posts, tags} =useSelector(state=>state.home)
 
-  console.log(posts,tags);
-
   const isPostsLoading = posts.status === 'loading';
   const isTagsLoading = tags.status === 'loading';
 
