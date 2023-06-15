2db1048fe45a081b73ce8c16e76871a6fe69decc
nazarBl <nazar1998777718@gmail.com>
2023-06-15 20:37:40 +0300

    Small fix set default avatar

--- client/src/components/UserInfo/index.jsx
+++ client/src/components/UserInfo/index.jsx
@@ -4,7 +4,7 @@ import style from './UserInfo.module.scss'
 export const UserInfo = ({avatarUrl, fullName, extraInfo}) => {
   return (
     <div className={style.root}>
-        <img className={style.avatar} src={`http://localhost:7000${avatarUrl}` || '/noavatar.png'} alt={fullName} />
+        <img className={style.avatar} src={avatarUrl? `http://localhost:7000${avatarUrl}` : '/noavatar.png'} alt={fullName} />
         <div className={style.userDetails}>
             <span className={style.userName}>{fullName}</span>
             <span className={style.extra}>{extraInfo}</span>
